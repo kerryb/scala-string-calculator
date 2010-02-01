1b99840ad9740649cfd9bb38cca0d2b0165c21fb
Kerry Buckley <kerryjbuckley@gmail.com>
2010-02-01 22:33:49 +0000

    Throw exception with negative input

--- src/StringCalculator.scala
+++ src/StringCalculator.scala
@@ -21,6 +21,10 @@ object StringCalculator {
   }
 
   private def addNumbers(numbers: Array[Int]) = {
+    val negatives = numbers.filter(n => n < 0)
+    if (!negatives.isEmpty) {
+      throw new NegativeInputException
+    }
     numbers.foldLeft(0) { (total, n) => total + n }
   }
 }
