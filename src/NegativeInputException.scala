ee7307b3c5a02ec0bbd9ec4bef707be7eca4d94e
Kerry Buckley <kerryjbuckley@gmail.com>
2010-02-02 09:57:45 +0000

    Step 5 complete
    Split long line

--- src/NegativeInputException.scala
+++ src/NegativeInputException.scala
@@ -1,2 +1,4 @@
 class NegativeInputException(badNumbers: Array[Int])
-  extends Exception("negatives not allowed: " + badNumbers.map((a) => a.toString).reduceLeft((a, b) => a + ", " + b))
+  extends Exception("negatives not allowed: " + badNumbers.map(
+    (a) => a.toString).reduceLeft(
+    (a, b) => a + ", " + b))
