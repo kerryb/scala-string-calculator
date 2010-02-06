6904f8392b323a66c0167520d21f73935c5afe4f
Kerry Buckley <kerryjbuckley@gmail.com>
2010-02-06 09:40:56 +0000

    Convert private methods to local

--- src/StringCalculator.scala
+++ src/StringCalculator.scala
@@ -1,38 +1,41 @@
+import scala.util.matching.Regex
+
 object StringCalculator {
   def add(input: String) = {
+    def extractNumbers(input: String) = {
+      def removeHeader(input: String) = {
+        input.replaceFirst("^//.*\n", "")
+      }
+
+      removeHeader(input).split(separator(input)).map((a) => a.toInt)
+    }
+
+    def separator(input: String) = {
+      def customSeparator(input: String) = {
+        val separatorRegex = "(?s)//(.).*".r
+        val separatorRegex(separator) = input
+        separator
+      }
+
+      if (input startsWith("//")) { customSeparator(input) } else { "[,\n]" }
+    }
+
+    def addNumbers(numbers: Array[Int]) = {
+      def failOnNegativeNumbers(numbers: Array[Int]) = {
+        val negatives = numbers.filter(n => n < 0)
+          if (!negatives.isEmpty) {
+          throw new NegativeInputException(negatives)
+        }
+      }
+
+      failOnNegativeNumbers(numbers)
+      numbers.foldLeft(0) { (total, n) => total + n }
+    }
+
     if (input isEmpty) {
       0
     } else {
-      val numbers = extractNumbers(input)
-      addNumbers(numbers)
-    }
-  }
-
-  private def extractNumbers(input: String) = {
-    removeHeader(input).split(separator(input)).map((a) => a.toInt)
-  }
-
-  private def removeHeader(input: String) = {
-    input.replaceFirst("^//.*\n", "")
-  }
-
-  private def separator(input: String) = {
-    if (input startsWith("//")) { customSeparator(input) } else { "[,\n]" }
-  }
-
-  private def customSeparator(input: String) = {
-    ";"
-  }
-
-  private def addNumbers(numbers: Array[Int]) = {
-    failOnNegativeNumbers(numbers)
-    numbers.foldLeft(0) { (total, n) => total + n }
-  }
-
-  private def failOnNegativeNumbers(numbers: Array[Int]) = {
-    val negatives = numbers.filter(n => n < 0)
-    if (!negatives.isEmpty) {
-      throw new NegativeInputException(negatives)
+      addNumbers(extractNumbers(input))
     }
   }
 }
